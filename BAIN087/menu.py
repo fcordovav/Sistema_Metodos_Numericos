3793dbda65a9d5bcf041688516d71bdb4a7fe36f
Felipe Córdova <85969736+fcordovav@users.noreply.github.com>
2023-12-10 16:54:28 -0300

    Delete BAIN087 directory

--- BAIN087/menu.py
+++
@@ -1,6 +0,0 @@
-def mostrarMenu():
-    with open("db/menu.txt", "r", encoding = "utf-8") as archivo:
-        lineas = archivo.readlines()
-                            
-        for linea in lineas:
-            print(f"\t{linea.strip()}")
